38ee9fda2d22561d487e8b525afd542baf21fc7a
Rowun Giles <rowun@rowungiles.com>
2017-04-03 10:55:43 +0100

    Initial Commit

---
+++ src/algorithm.hpp
@@ -0,0 +1,11 @@
+#ifndef HeckelDiff_H
+#define HeckelDiff_H
+
+class HeckelDiff {
+
+public:
+    static bool helloWorld() { return true; }
+};
+
+
+#endif //${name}
---
+++ test/algorithm_tests.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "../src/algorithm.hpp"
+
+TEST(HeckelDiff, HelloWorld) {
+    EXPECT_TRUE(HeckelDiff::helloWorld());
+}
